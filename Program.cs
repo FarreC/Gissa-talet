4c3cb087a5f65a1b9872b12b8f8943c90da753e8
Farre Cosmin <97farcos@skola.boras.se>
2019-09-29 23:50:01 +0200

    Guess the number

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GissaTalet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random rand = new Random();
+            int randTal = rand.Next(1,50);
+            int talInt = 0;
+            int points = 0;
+            Console.WriteLine("Hej! Gissa talet mellan 1-50");
+            
+            while(true)
+            {
+                
+                string talet = Console.ReadLine();
+
+                try
+                {
+                    talInt = Convert.ToInt32(talet);
+                }
+
+                catch(Exception)
+                { 
+                    Console.WriteLine("Felmeddelande: Skriv in endast siffror");       
+                    continue;
+                }   
+                
+                if(talInt == randTal)
+                {
+                    points++;
+                    Console.WriteLine($"Grattis, du gissade rätt, talet var {talInt}. Du försökte {points} antal gånger");
+                    break;
+
+                }
+                else if(talInt > randTal)
+                {
+                    Console.WriteLine("Lägre");
+                    points++;
+                    
+                }
+                else if(talInt < randTal)
+                {
+                    Console.WriteLine("Högre");
+                    points++;
+                    
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Gissa-talet
+
+## Komplex övning som tränar dig i fler moment samtidigt. 
+
+I denna uppgift ska du skapa ett klassiskt spel. Spelet går till så att datorn slumpvis hittar på ett tal mellan 1 och 100. Användaren ska sedan gissa talet.
+
+Gissar man fel svarar programmet "Fel. Talet är mindre." respektive "Fel. Talet är större." Gissar du rätt tal skriver programmet ut en grattis fras.
+I slutet skriver programmet ut antalet försök. Programmet ska hantera undantag dvs skriver man något annat än ett tal ska programmet hantera detta på lämpligt sätt.
+
+Börja gärna med att skriva pseudokod och gör ett aktivitetsdiagram. För att klara uppgiften behöver du ta reda på hur man slumpar fram tal i C#. 
